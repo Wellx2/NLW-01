1821dde629926ad8c3b4f6fd4635666e9e579b49
Wellington S. Tavares <wsilvatavares@outlook.com>
2020-06-19 22:11:28 -0300

    App

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Routes from'./routes'
+
+function App() {
+// [valor do estado, função para atualizar o  valor do estado]
+
+  return (
+    <Routes />
+  );
+}
+
+export default App;
